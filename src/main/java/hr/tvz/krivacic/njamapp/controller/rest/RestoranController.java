5202ca7d57df86133e8ccd16e64aa1f1186868f6
antekrivi <ante.krivacic@gmail.com>
2025-03-24 00:32:03 +0100

    Prva priprema

--- src/main/java/hr/tvz/krivacic/njamapp/controller/rest/RestoranController.java
+++ src/main/java/hr/tvz/krivacic/njamapp/controller/rest/RestoranController.java
@@ -3,28 +3,32 @@ package hr.tvz.krivacic.njamapp.controller.rest;
 import hr.tvz.krivacic.njamapp.model.RestoranDTO;
 import hr.tvz.krivacic.njamapp.model.RestoranService;
 import hr.tvz.krivacic.njamapp.model.RestoranServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("rest/restoran")
+@RequestMapping("restoran")
 public class RestoranController {
+    private static final Logger logger = LoggerFactory.getLogger(RestoranController.class);
     private final RestoranServiceImpl restoranServiceImpl;
 
     public RestoranController(RestoranServiceImpl restoranServiceImpl) {
         this.restoranServiceImpl = restoranServiceImpl;
     }
 
-    @GetMapping("/all")
+    @GetMapping()
     public List<RestoranDTO> getAllRestorani(){
         return restoranServiceImpl.findAll();
     }
 
-    @GetMapping("/id")
-    public RestoranDTO getRestoranById(Long id){
+    @GetMapping(params = "id")
+    public RestoranDTO getRestoranById(@RequestParam final Long id){
         return restoranServiceImpl.findRestoranByID(id);
     }
 
